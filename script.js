eddfb8086dfef5e37936d7a9a42c535eac8b3788
Dino <dleootta@gmail.com>
2025-04-22 16:51:07 +0200

    2- Created Computer and Human's Choices

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// PROJECT STARTED ON TUES. 22TH AT 00.30AM //
+
 // COMPUTER'S CHOICE
 
 function getComputerChoice() {
   const a = Math.random();
   if (a < 0.33) {
-    console.log(`Rock`);
+    console.log(`COMPUTER'S CHOICE : Rock`);
   } else if (a < 0.66) {
-    console.log(`Paper`);
+    console.log(`COMPUTER'S CHOICE : Paper`);
   } else {
-    console.log(`Scissors`);
+    console.log(`COMPUTER'S CHOICE : Scissors`);
   }
 }
 getComputerChoice();
+
+// HUMAN'S CHOICE
+
+function getHumanChoice() {
+  let userInput = prompt(`Rock, Paper or Scissors ?`);
+  console.log(`HUMAN'S CHOICE : `, userInput);
+}
+
+getHumanChoice();
